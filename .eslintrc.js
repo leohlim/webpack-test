7e76df4c757c39a2fb6e34ebaf83cd053c1f0d17
leohlim <leohlim@gmail.com>
2023-01-08 20:05:46 -0600

    completed tutorial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": "eslint:recommended",
     "overrides": [
     ],
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+
+
     },
     "rules": {
-    }
+        
+    }, 
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import myName from './myName';
+import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-    // use function
-    element.innerHTML = myName('Cody');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
+  btn.innerHTML = 'Click me and check on the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
 }
 
 document.body.appendChild(component());
